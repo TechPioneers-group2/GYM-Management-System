b4f06de971acb1787bd880536a5a28b961ecdd17
AlHareth alhyari <hdhareth@gmail.com>
2023-08-19 15:31:29 +0300

    Mock & Xunit Tests For Supplements Service

---
+++ GYM-Management-System/Gym-System_test/SupplementsTests.cs
@@ -0,0 +1,44 @@
+﻿using GYM_Management_System.Models.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_System_test
+{
+    public class SupplementsTests : Mock
+    {
+
+        [Fact]
+        public async Task CanAddASupplementToDataBase()
+        {
+            // Arrange
+            var supplement = await CreateAndSaveSupplementTest();
+
+            // Act
+            var supplementService = new SupplementService(_db);
+
+            // Assert
+            var actualSupplement = await supplementService.GetSupplementById(supplement.SupplementID);
+            Assert.Equal(supplement.SupplementID, actualSupplement.SupplementID);
+            Assert.Equal(supplement.Name, actualSupplement.Name);
+            Assert.Equal(supplement.Price, actualSupplement.Price);
+        }
+
+        [Fact]
+        public async Task CanAddAndDeleteSupplementFromDataBase()
+        {
+            // Arrange
+            var supplement = await CreateAndSaveSupplementTest();
+            var supplementService = new SupplementService(_db);
+
+            // Act
+            await supplementService.DeleteSupplement(supplement.SupplementID);
+
+            // Assert
+            var deletedSupplement = await supplementService.GetSupplementById(supplement.SupplementID);
+            Assert.Null(deletedSupplement);
+        }
+    }
+}
