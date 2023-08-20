e38d87fd768e07b2abf3417709f9178020e7bbcd
alaaabusalem <alaa.abusalem94@hotmail.com>
2023-08-20 11:37:25 +0300

    finishing Employee test

--- GYM-Management-System/GYM-Management-System/Models/Services/EmployeeService.cs
+++ GYM-Management-System/GYM-Management-System/Models/Services/EmployeeService.cs
@@ -5,21 +5,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GYM_Management_System.Models.Services
 {
-    public class EmployeeService : IEmployee
-    {
-        private readonly GymDbContext _db;
+	public class EmployeeService : IEmployee
+	{
+		private readonly GymDbContext _db;
 
-        public EmployeeService(GymDbContext db)
-        {
-            _db = db;
-        }
+		public EmployeeService(GymDbContext db)
+		{
+			_db = db;
+		}
 
 		public async Task<EmployeeDTO> Create(CreatEmployeeDTO creatEmployeeDTO)
 		{
 			var gym = await _db.Gyms.FindAsync(creatEmployeeDTO.GymID);
-			if(gym==null)
+			if (gym == null)
 			{
-				return null;	
+				return null;
 			}
 			var newEmployee = new Employee()
 			{
@@ -35,7 +35,7 @@ namespace GYM_Management_System.Models.Services
 			await _db.SaveChangesAsync();
 			EmployeeDTO employeeDTO = new EmployeeDTO()
 			{
-				EmployeeID= newEmployee.EmployeeID,
+				EmployeeID = newEmployee.EmployeeID,
 				GymID = creatEmployeeDTO.GymID,
 				Name = creatEmployeeDTO.Name,
 				JobDescription = creatEmployeeDTO.JobDescription,
@@ -43,125 +43,129 @@ namespace GYM_Management_System.Models.Services
 				WorkingDays = creatEmployeeDTO.WorkingDays,
 				WorkingHours = creatEmployeeDTO.WorkingHours,
 				Salary = creatEmployeeDTO.Salary,
-			}; 
+			};
 			return employeeDTO;
 		}
 
-        public async Task Delete(int employeeId)
-        {
-            Employee employee = await _db.Employees.FindAsync(employeeId);
-            if (employee != null)
-            {
-                _db.Employees.Remove(employee);
-                await _db.SaveChangesAsync();
-            }
+		public async Task Delete(int employeeId)
+		{
+			Employee employee = await _db.Employees.FindAsync(employeeId);
+			if (employee != null)
+			{
+				_db.Employees.Remove(employee);
+				await _db.SaveChangesAsync();
+			}
 
-        }
+		}
 
-        public async Task<EmployeeDTO> GetEmployee(int id)
-        {
-            var employee = await _db.Employees
-                .FirstOrDefaultAsync(em => em.EmployeeID == id);
-            var employeeDTO = new EmployeeDTO()
-            {
-                EmployeeID = employee.EmployeeID,
-                GymID = employee.GymID,
-                Name = employee.Name,
-                JobDescription = employee.JobDescription,
-                IsAvailable = employee.IsAvailable,
-                WorkingDays = employee.WorkingDays,
-                WorkingHours = employee.WorkingHours,
-                Salary = employee.Salary,
-            };
+		public async Task<EmployeeDTO> GetEmployee(int id)
+		{
+			var employee = await _db.Employees
+				.FirstOrDefaultAsync(em => em.EmployeeID == id);
+			if (employee == null)
+			{
+				return null;
+			}
+			var employeeDTO = new EmployeeDTO()
+			{
+				EmployeeID = employee.EmployeeID,
+				GymID = employee.GymID,
+				Name = employee.Name,
+				JobDescription = employee.JobDescription,
+				IsAvailable = employee.IsAvailable,
+				WorkingDays = employee.WorkingDays,
+				WorkingHours = employee.WorkingHours,
+				Salary = employee.Salary,
+			};
 
-            return employeeDTO;
-        }
+			return employeeDTO;
+		}
 
-        public async Task<List<EmployeeDTO>> GetEmployees()
-        {
-            List<Employee> employees = await _db.Employees
-                .Include(em => em.Gym).ToListAsync();
-            if (employees != null)
-            {
-                List<EmployeeDTO> employeeDTOs = new List<EmployeeDTO>();
-                foreach (Employee employee in employees)
-                {
-                    EmployeeDTO employeeDTO = new EmployeeDTO()
-                    {
-                        EmployeeID = employee.EmployeeID,
-                        GymID = employee.GymID,
-                        Name = employee.Name,
-                        JobDescription = employee.JobDescription,
-                        WorkingDays = employee.WorkingDays,
-                        WorkingHours = employee.WorkingHours,
-                        Salary = employee.Salary,
-                        IsAvailable = employee.IsAvailable
-                    };
-                    employeeDTOs.Add(employeeDTO);
-                }
-                return employeeDTOs;
-            }
-            return null;
-        }
+		public async Task<List<EmployeeDTO>> GetEmployees()
+		{
+			List<Employee> employees = await _db.Employees
+				.Include(em => em.Gym).ToListAsync();
+			if (employees != null)
+			{
+				List<EmployeeDTO> employeeDTOs = new List<EmployeeDTO>();
+				foreach (Employee employee in employees)
+				{
+					EmployeeDTO employeeDTO = new EmployeeDTO()
+					{
+						EmployeeID = employee.EmployeeID,
+						GymID = employee.GymID,
+						Name = employee.Name,
+						JobDescription = employee.JobDescription,
+						WorkingDays = employee.WorkingDays,
+						WorkingHours = employee.WorkingHours,
+						Salary = employee.Salary,
+						IsAvailable = employee.IsAvailable
+					};
+					employeeDTOs.Add(employeeDTO);
+				}
+				return employeeDTOs;
+			}
+			return null;
+		}
 
-        public async Task<List<GetEmployeesByGymId>> GetEmployeesByGymId(int gymId)
-        {
-            List<Employee> employees = await _db.Employees
-                .Where(em => em.GymID == gymId)
-                .ToListAsync();
-            if (employees != null)
-            {
-                List<GetEmployeesByGymId> employeesByIds = new List<GetEmployeesByGymId>();
-                foreach (Employee employee in employees)
-                {
-                    GetEmployeesByGymId em = new GetEmployeesByGymId()
-                    {
-                        EmployeeID = employee.EmployeeID,
-                        Name = employee.Name,
-                        JobDescription = employee.JobDescription,
-                        IsAvailable = employee.IsAvailable,
-                        WorkingDays = employee.WorkingDays,
-                        WorkingHours = employee.WorkingHours,
-                        Salary = employee.Salary,
-                    };
-                    employeesByIds.Add(em);
-                }
-                return employeesByIds;
-            }
-            return null;
-        }
+		public async Task<List<GetEmployeesByGymId>> GetEmployeesByGymId(int gymId)
+		{
+			List<Employee> employees = await _db.Employees
+				.Where(em => em.GymID == gymId)
+				.ToListAsync();
+			if (employees != null)
+			{
+				List<GetEmployeesByGymId> employeesByIds = new List<GetEmployeesByGymId>();
+				foreach (Employee employee in employees)
+				{
+					GetEmployeesByGymId em = new GetEmployeesByGymId()
+					{
+						EmployeeID = employee.EmployeeID,
+						Name = employee.Name,
+						JobDescription = employee.JobDescription,
+						IsAvailable = employee.IsAvailable,
+						WorkingDays = employee.WorkingDays,
+						WorkingHours = employee.WorkingHours,
+						Salary = employee.Salary,
+					};
+					employeesByIds.Add(em);
+				}
+				return employeesByIds;
+			}
+			return null;
+		}
 
 
-        public async Task<EmployeeDTO> Update(UpdateEmployeeDTO updateEmployeeDTO, int employeeId)
-        {
-            Employee employee = await _db.Employees.FindAsync(employeeId);
-            if (employee != null)
-            {
-                employee.GymID = updateEmployeeDTO.GymID;
-                employee.Name = updateEmployeeDTO.Name;
-                employee.JobDescription = updateEmployeeDTO.JobDescription;
-                employee.WorkingDays = updateEmployeeDTO.WorkingDays;
-                employee.WorkingHours = updateEmployeeDTO.WorkingHours;
-                employee.Salary = updateEmployeeDTO.Salary;
-                employee.IsAvailable = updateEmployeeDTO.IsAvailable;
-                _db.Entry(employee).State = EntityState.Modified;
-                await _db.SaveChangesAsync();
+		public async Task<EmployeeDTO> Update(UpdateEmployeeDTO updateEmployeeDTO, int employeeId)
+		{
+			Employee employee = await _db.Employees.FindAsync(employeeId);
+			if (employee != null)
+			{
+				employee.GymID = updateEmployeeDTO.GymID;
+				employee.Name = updateEmployeeDTO.Name;
+				employee.JobDescription = updateEmployeeDTO.JobDescription;
+				employee.WorkingDays = updateEmployeeDTO.WorkingDays;
+				employee.WorkingHours = updateEmployeeDTO.WorkingHours;
+				employee.Salary = updateEmployeeDTO.Salary;
+				employee.IsAvailable = updateEmployeeDTO.IsAvailable;
+				_db.Entry(employee).State = EntityState.Modified;
+				await _db.SaveChangesAsync();
 
-                EmployeeDTO employeeDTO = new EmployeeDTO()
-                {
-                    EmployeeID = employeeId,
-                    GymID = updateEmployeeDTO.GymID,
-                    Name = updateEmployeeDTO.Name,
-                    JobDescription = updateEmployeeDTO.JobDescription,
-                    IsAvailable = updateEmployeeDTO.IsAvailable,
-                    WorkingDays = updateEmployeeDTO.WorkingDays,
-                    WorkingHours = updateEmployeeDTO.WorkingHours,
-                    Salary = updateEmployeeDTO.Salary,
-                };
+				EmployeeDTO employeeDTO = new EmployeeDTO()
+				{
+					EmployeeID = employeeId,
+					GymID = updateEmployeeDTO.GymID,
+					Name = updateEmployeeDTO.Name,
+					JobDescription = updateEmployeeDTO.JobDescription,
+					IsAvailable = updateEmployeeDTO.IsAvailable,
+					WorkingDays = updateEmployeeDTO.WorkingDays,
+					WorkingHours = updateEmployeeDTO.WorkingHours,
+					Salary = updateEmployeeDTO.Salary,
+				};
 
-                return employeeDTO;
-            }
-            return null;
-        }
-    }
+				return employeeDTO;
+			}
+			return null;
+		}
+	}
 }
--- GYM-Management-System/Gym-System_test/Mock.cs
+++ GYM-Management-System/Gym-System_test/Mock.cs
@@ -1,106 +1,152 @@
 using GYM_Management_System.Data;
 using GYM_Management_System.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using GYM_Management_System.Models.DTOs;
+using GYM_Management_System.Models.Interfaces;
 
 namespace Gym_System_test
 {
-    public class Mock : IDisposable
-    {
-        private readonly SqliteConnection _connection;
-        protected readonly GymDbContext _db;
+	public class Mock : IDisposable
+	{
+		private readonly SqliteConnection _connection;
+		protected readonly GymDbContext _db;
 
-        public Mock()
-        {
-            _connection = new SqliteConnection("Filename=:memory:");
-            _connection.Open();
+		public Mock()
+		{
+			_connection = new SqliteConnection("Filename=:memory:");
+			_connection.Open();
 
-            _db = new GymDbContext(
-                  new DbContextOptionsBuilder<GymDbContext>()
-                  .UseSqlite(_connection).Options);
-
-            _db.Database.EnsureCreated();
-        }
-
-        protected async Task<Supplement> CreateAndSaveSupplementTest()
-        {
-            var supplement = new Supplement()
-            {
-                Name = "Test",
-                Price = "Test",
-            };
-            _db.Supplements.Add(supplement);
-            await _db.SaveChangesAsync();
-
-            Assert.NotEqual(0, supplement.SupplementID);
-            return supplement;
-        }
-        protected async Task DeleteSupplementForTest(int supplementId)
-        {
-            var supplementToDelete = await _db.Supplements.FindAsync(supplementId);
-            if (supplementToDelete != null)
-            {
-                _db.Supplements.Remove(supplementToDelete);
-                await _db.SaveChangesAsync();
-            }
-        }
-
-        protected async Task UpdateSupplementForTest(int supplementId, string newName, string newPrice)
-        {
-            var supplementToUpdate = await _db.Supplements.FindAsync(supplementId);
-            if (supplementToUpdate != null)
-            {
-                supplementToUpdate.Name = newName;
-                supplementToUpdate.Price = newPrice;
-                _db.Entry(supplementToUpdate).State = EntityState.Modified;
-                await _db.SaveChangesAsync();
-            }
-        }
-        protected async Task<GymEquipment> CreateAndSaveGymEquipmentTest()
-        {
-            var gymEquipment = new GymEquipment()
-            {
-                Name = "Test Equipment",
-                Quantity = 10,
-                OutOfService = 0,
-                GymID = 1 // Change this to the appropriate GymID
-            };
-
-            _db.GymEquipments.Add(gymEquipment);
-            await _db.SaveChangesAsync();
-
-            Assert.NotEqual(0, gymEquipment.GymEquipmentID);
-            return gymEquipment;
-        }
-
-        protected async Task DeleteGymEquipmentForTest(int gymEquipmentId)
-        {
-            var gymEquipmentToDelete = await _db.GymEquipments.FindAsync(gymEquipmentId);
-            if (gymEquipmentToDelete != null)
-            {
-                _db.GymEquipments.Remove(gymEquipmentToDelete);
-                await _db.SaveChangesAsync();
-            }
-        }
-
-        protected async Task UpdateGymEquipmentForTest(int gymEquipmentId, int newQuantity, int newOutOfService)
-        {
-            var gymEquipmentToUpdate = await _db.GymEquipments.FindAsync(gymEquipmentId);
-            if (gymEquipmentToUpdate != null)
-            {
-                gymEquipmentToUpdate.Quantity = newQuantity;
-                gymEquipmentToUpdate.OutOfService = newOutOfService;
-                _db.Entry(gymEquipmentToUpdate).State = EntityState.Modified;
-                await _db.SaveChangesAsync();
-            }
-        }
+			_db = new GymDbContext(
+			new DbContextOptionsBuilder<GymDbContext>()
+			.UseSqlite(_connection).Options);
 
 
+			_db.Database.EnsureCreated();
 
-        public void Dispose()
-        {
-            _db?.Dispose();
-            _connection?.Dispose();
-        }
-    }
+		}
+
+		protected async Task CreateAndSaveEmplyee()
+		{
+
+			var Employee1 = new Employee()
+			{
+				UserId = "1",
+				GymID = 1,
+				Name = "test1",
+				JobDescription = "Instructor",
+				IsAvailable = true,
+				WorkingDays = "test1",
+				WorkingHours = "test1",
+				Salary = "test1"
+			};
+			await _db.Employees.AddAsync(Employee1);
+			await _db.SaveChangesAsync();
+
+
+			var Employee2 = new Employee()
+			{
+				UserId = "2",
+				GymID = 1,
+				Name = "test2",
+				JobDescription = "Instructor",
+				IsAvailable = false,
+				WorkingDays = "test2",
+				WorkingHours = "test2",
+				Salary = "test2"
+			};
+			await _db.Employees.AddAsync(Employee2);
+			await _db.SaveChangesAsync();
+			List<Employee> employees = new List<Employee>();
+			employees.Add(Employee1);
+			employees.Add(Employee2);
+			//return employees;
+
+		}
+		protected async Task<Supplement> CreateAndSaveSupplementTest()
+		{
+			var supplement = new Supplement()
+			{
+				Name = "Test",
+				Price = "Test",
+			};
+			_db.Supplements.Add(supplement);
+			await _db.SaveChangesAsync();
+
+			Assert.NotEqual(0, supplement.SupplementID);
+			return supplement;
+		}
+		protected async Task DeleteSupplementForTest(int supplementId)
+		{
+			var supplementToDelete = await _db.Supplements.FindAsync(supplementId);
+			if (supplementToDelete != null)
+			{
+				_db.Supplements.Remove(supplementToDelete);
+				await _db.SaveChangesAsync();
+			}
+		}
+
+		protected async Task UpdateSupplementForTest(int supplementId, string newName, string newPrice)
+		{
+			var supplementToUpdate = await _db.Supplements.FindAsync(supplementId);
+			if (supplementToUpdate != null)
+			{
+				supplementToUpdate.Name = newName;
+				supplementToUpdate.Price = newPrice;
+				_db.Entry(supplementToUpdate).State = EntityState.Modified;
+				await _db.SaveChangesAsync();
+			}
+		}
+		protected async Task<GymEquipment> CreateAndSaveGymEquipmentTest()
+		{
+			var gymEquipment = new GymEquipment()
+			{
+				Name = "Test Equipment",
+				Quantity = 10,
+				OutOfService = 0,
+				GymID = 1 // Change this to the appropriate GymID
+			};
+
+			_db.GymEquipments.Add(gymEquipment);
+			await _db.SaveChangesAsync();
+
+			Assert.NotEqual(0, gymEquipment.GymEquipmentID);
+			return gymEquipment;
+		}
+
+		protected async Task DeleteGymEquipmentForTest(int gymEquipmentId)
+		{
+			var gymEquipmentToDelete = await _db.GymEquipments.FindAsync(gymEquipmentId);
+			if (gymEquipmentToDelete != null)
+			{
+				_db.GymEquipments.Remove(gymEquipmentToDelete);
+				await _db.SaveChangesAsync();
+			}
+		}
+
+		protected async Task UpdateGymEquipmentForTest(int gymEquipmentId, int newQuantity, int newOutOfService)
+		{
+			var gymEquipmentToUpdate = await _db.GymEquipments.FindAsync(gymEquipmentId);
+			if (gymEquipmentToUpdate != null)
+			{
+				gymEquipmentToUpdate.Quantity = newQuantity;
+				gymEquipmentToUpdate.OutOfService = newOutOfService;
+				_db.Entry(gymEquipmentToUpdate).State = EntityState.Modified;
+				await _db.SaveChangesAsync();
+			}
+		}
+
+
+
+		public void Dispose()
+		{
+			_db?.Dispose();
+			_connection?.Dispose();
+		}
+	}
 }
