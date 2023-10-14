4b6a9f82827a8b0c593d40c1bdd17104c004029c
Nadine-ALmasri <128686456+Nadine-ALmasri@users.noreply.github.com>
2023-10-14 21:16:10 +0300

    Delete GYM-Management-System/Gym-Mangement-systemMVC directory
    edited

--- GYM-Management-System/Gym-Mangement-systemMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Gym_Mangement_systemMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
