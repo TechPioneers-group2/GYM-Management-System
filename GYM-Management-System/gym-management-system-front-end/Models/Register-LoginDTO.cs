4bcefa5d20a23956f004d71ddd915a40a2a15cef
Ala' Abusalem <23037404@student.ltuc.com>
2023-10-15 11:31:36 +0300

    edit viewmodel

--- GYM-Management-System/gym-management-system-front-end/Models/Register-LoginDTO.cs
+++ GYM-Management-System/gym-management-system-front-end/Models/Register-LoginDTO.cs
@@ -40,13 +40,13 @@
         public DateTime SubscriptionDate { get; set; }
         public DateTime SubscriptionExpiry { get; set; }
     }
-    public class UserDTO
-    {
-        public string Id { get; set; }
-        public string UserName { get; set; }
-        public string Token { get; set; }
-        public List<string> Roles { get; set; }
-    }
+   // public class UserDTO
+   // {
+    //    public string Id { get; set; }
+    //    public string UserName { get; set; }
+    //    public string Token { get; set; }
+    //    public List<string> Roles { get; set; }
+    //}
 
     public class LogInDTO
     {
