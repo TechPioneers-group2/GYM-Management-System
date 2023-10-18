a022e670612bd74643c8c5b26f5d36c4eaa60105
user <user@DESKTOP-7AK0NU9>
2023-10-18 07:21:43 +0300

    add equipments , subscriptionteirs , client to admin dashboard

--- GYM-Management-System/gym-management-system-front-end/Controllers/EquipmentController.cs
+++ GYM-Management-System/gym-management-system-front-end/Controllers/EquipmentController.cs
@@ -14,6 +14,20 @@ namespace gym_management_system_front_end.Controllers
             _httpClient.BaseAddress = BaseAdress;
         }
         [HttpGet]
+        public IActionResult TableView()
+        {
+            List<EquipmentViewModel> equipments = new List<EquipmentViewModel>();
+            HttpResponseMessage response = _httpClient.GetAsync(_httpClient.BaseAddress + "/GymEquipments/Get").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                equipments = JsonConvert.DeserializeObject<List<EquipmentViewModel>>(data);
+
+
+            }
+            return View(equipments);
+        }
+        [HttpGet]
         public IActionResult Index()
         {
             List<EquipmentViewModel> equipments = new List<EquipmentViewModel>();
