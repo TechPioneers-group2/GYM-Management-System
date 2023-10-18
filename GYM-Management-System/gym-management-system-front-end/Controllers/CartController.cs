4b377d56488ee779b0126e638555acdebce58d4b
Ammar <100584308+AMMARALBESANI@users.noreply.github.com>
2023-10-18 23:20:09 +0300

    shopping cart 2

--- GYM-Management-System/gym-management-system-front-end/Controllers/CartController.cs
+++ GYM-Management-System/gym-management-system-front-end/Controllers/CartController.cs
@@ -1,10 +1,13 @@
-﻿using gym_management_system_front_end.Models;
+﻿using Azure;
+using GYM_Management_System.Models;
+using gym_management_system_front_end.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Newtonsoft.Json;
 using System.Linq;
 using System.Text;
 using System.Web;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace gym_management_system_front_end.Controllers
 {
@@ -21,82 +24,107 @@ namespace gym_management_system_front_end.Controllers
             _client = new HttpClient();
             _client.BaseAddress = baseAddress;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var cart = HttpContext.Request.Cookies["SupplementCart"];
+            var dicCart = JsonConvert.DeserializeObject<Dictionary<int, int>>(cart);
+            var cartItems = new List<CartViewModel>();
 
-            var sup = GetCartFromCookies();
+            foreach(var item in dicCart)
+            {
+                var res = await _client.GetAsync(_client.BaseAddress + "/GetSupplement/" + item.Key);
+                var jsondata = await res.Content.ReadAsStringAsync();
+                var product = JsonConvert.DeserializeObject<SupplementViewModel>(jsondata);
+                int y;
+                dicCart.TryGetValue(product.SupplementID,out y);
+                var x = new CartViewModel
+                {
+                    SupplementID = product.SupplementID,
+                    imageURL=product.imageURL,
+                    Description=product.Description,
+                    Price=product.Price,
+                    Name=product.Name,
+                    Quantity=y
+                };
+                cartItems.Add(x);
+            }
 
-            return View(sup);
+            return View(cartItems);
+            
         }
 
         [HttpPost]
         public async Task<ActionResult> AddToCart(int supplementID)
         {
 
-            // Retrieve cart items from cookies
-            var cart = GetCartFromCookies(); 
-          
-            var supplement = new SupplementViewModel();
-            var response = await _client.GetAsync(_client.BaseAddress + "/GetSupplement/" + supplementID);
-            
-            if (response.IsSuccessStatusCode)
-            {
-                string data = await response.Content.ReadAsStringAsync();
-                supplement = JsonConvert.DeserializeObject<SupplementViewModel>(data);
-            }
-
-            
-                cart.Add(supplement);
-
-                // Add the new item to the cart
-                SaveCartToCookies(cart);
-
-            
-            
-
-               
-            
-
-            return Redirect("/#Supplements");
-        }
-        private List<SupplementViewModel> GetCartFromCookies()
-        {
-            var cartCookie = HttpContext.Request.Cookies["ShoppingCart"];
+            var cartCookie = HttpContext.Request.Cookies["SupplementCart"];
+            Dictionary<int, int> supplement;
             if (cartCookie != null)
             {
-                var cartJson = HttpUtility.UrlDecode(cartCookie);
-                return JsonConvert.DeserializeObject<List<SupplementViewModel>>(cartJson);
+                supplement = JsonConvert.DeserializeObject<Dictionary<int, int>>(cartCookie);
+
+
+                if (supplement.ContainsKey(supplementID))
+                {
+                    supplement[supplementID] += 1;
+                }
+                else
+                {
+                    supplement[supplementID] = 1;
+                }
+
             }
-            return new List<SupplementViewModel>();
+            else
+            {
+                supplement = new Dictionary<int, int>
+                   {
+                       { supplementID, 1 }
+                   };
+            }
+
+            var option = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(7)
+
+            };
+            HttpContext.Response.Cookies.Append("SupplementCart", JsonConvert.SerializeObject(supplement),option) ;
+
+            string refURL = Request.Headers["Referer"].ToString();
+
+            return Redirect(refURL);
         }
+
 
         public ActionResult RemoveFromCart(int supplementID)
         {
-            // Retrieve cart items from cookies
-            var cart = GetCartFromCookies();
-
-            // Remove the item from the cart
-            var itemToRemove = cart.FirstOrDefault(x=>x.SupplementID== supplementID);
-            if (itemToRemove != null)
+            var removed = HttpContext.Request.Cookies["SupplementCart"];
+            Dictionary<int, int> supplement;
+            if (removed != null)
             {
-                cart.Remove(itemToRemove);
+                supplement = JsonConvert.DeserializeObject<Dictionary<int, int>>(removed);
+
+
+                if (supplement.ContainsKey(supplementID))
+                {
+                    supplement[supplementID] -= 1;
+                }
+                if (supplement[supplementID] <= 0)
+                {
+                    supplement.Remove(supplementID);
+                }
+
+                var option = new CookieOptions
+                {
+                    Expires = DateTime.Now.AddDays(7)
+
+                };
+                HttpContext.Response.Cookies.Append("SupplementCart", JsonConvert.SerializeObject(supplement), option);
             }
+            string refURL = Request.Headers["Referer"].ToString();
 
-            // Save the updated cart to cookies
-            SaveCartToCookies(cart);
+            return Redirect(refURL);
 
-            return RedirectToAction("Index");
         }
 
-
-        private void SaveCartToCookies(List<SupplementViewModel> supplements)
-        {
-            var supplementsJson = JsonConvert.SerializeObject(supplements);
-            var cookieOptions = new CookieOptions
-            {
-                Expires = DateTime.Now.AddDays(7) // Adjust the expiration as needed
-            };
-            Response.Cookies.Append("ShoppingCart", HttpUtility.UrlEncode(supplementsJson), cookieOptions);
-        }
     }
 }
---
+++ GYM-Management-System/gym-management-system-front-end/Models/CartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace gym_management_system_front_end.Models
+{
+    public class CartViewModel
+    {
+
+        [DisplayName("Supplement ID ")]
+        public int SupplementID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }   
+        public string? imageURL { get; set; }
+    }
+}
